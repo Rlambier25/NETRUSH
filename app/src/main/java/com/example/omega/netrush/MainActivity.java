1075f0a003d629ab69542e683fd83a47b8fe5dcf
Reed Lambier <rlambier46@gmail.com>
2016-08-04 13:53:04 -0700

    Initial commit

---
+++ app/src/main/java/com/example/omega/netrush/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.omega.netrush;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.Bind;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.clientButton) Button mClientButton;
+    @Bind(R.id.aboutButton) Button mAboutButton;
+    @Bind(R.id.introText) TextView mIntroText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Typeface introFont = Typeface.createFromAsset(getAssets(), "fonts/Colleged.ttf");
+        mIntroText.setTypeface(introFont);
+
+        mClientButton.setOnClickListener(this);
+        mAboutButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mAboutButton){
+            Intent goToAbout = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(goToAbout);
+        }
+
+        if(v == mClientButton) {
+            Intent goToClient = new Intent(MainActivity.this, ClientActivity.class);
+            startActivity(goToClient);
+        }
+    }
+}
